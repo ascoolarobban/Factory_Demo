b90d2a1abfb834032972cebd75fe4688342b6dea
ascoolarobban <robin@fauxdelorean.com>
2023-02-28 22:18:44 +0100

    Initial Commit

---
+++ Factory_Demo/Factory_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Factory_DemoApp.swift
+//  Factory_Demo
+//
+//  Created by Robin Ellingsen on 2023-02-28.
+//
+
+import SwiftUI
+
+@main
+struct Factory_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
